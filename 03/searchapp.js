1113734f1624b38c134cb8c0ee86caad92adf820
Wef2 <qhrud5725@hanmail.net>
2015-04-09 01:26:30 +0900

    03 UPDATE Movie / Search API Example

--- 03/searchapp.js
+++ 03/searchapp.js
@@ -1,10 +1,53 @@
-var daumSearchAPI = 'https://apis.daum.net/search/web?apikey=39561f0a539d9eff4af3f079ecdc460a&q=카카오프렌즈 -네오&output=json';
-var listTemplate = $('listTemplate').innerHTML;
 
-getJSON(daumSearchAPI , function(daumSearchData){
+var count;
+var keyword;
+var searchButton = $('searchButton');
+var searchInput = $('searchInput');
 
-  console.log(daumSearchData)
-  var html = tmpl(listTemplate, {list: daumSearchData.channel.item});
-  $('divSearch').innerHTML = html;
+searchButton.addEventListener('click',showResult);
+searchButton.addEventListener('click',showMoreButton);
 
-});
+function showJSON(){
+	var daumSearchAPI = 'https://apis.daum.net/search/web?apikey=39561f0a539d9eff4af3f079ecdc460a&q='+keyword+'&output=json&result='+count;
+	var listTemplate = $('listTemplate').innerHTML;
+
+	getJSON(daumSearchAPI , function(daumSearchData){
+	var str = tmpl(listTemplate, {list: daumSearchData.channel.item});	
+	str1 = str.replace(/&lt;/g, "<");
+	html = str1.replace(/&gt;/g, ">");
+
+	$('divResult').innerHTML = html;
+	});
+
+}
+
+function showResult(){
+
+	count = 10;	
+	keyword = searchInput.value;
+
+	showJSON();
+}
+
+function showMoreButton(){
+
+	var divMore = $('divMore');
+	if (divMore.hasChildNodes()) {
+    divMore.removeChild(divMore.childNodes[0]);
+	}
+	var moreButton = document.createElement('button');
+	moreButton.addEventListener("click",showMore);
+	moreButton.style.margin = "10px 350px 10px 350px"
+	moreButton.style.width = "100px";
+	moreButton.style.height = "30px";
+	moreButton.innerHTML = "더 보기"
+
+	divMore.appendChild(moreButton);
+}
+
+function showMore(){
+
+	count = count + 10;
+	showJSON();
+
+}
